77d2034dcafa45db728734d9af49c294155e3275
caytro <snadaud@free.fr>
2021-12-24 17:59:44 +0100

    Apres passage PHP8

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,14 +8,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return new Response(<<<EOF
             <html>
                 <body>
+                    <h1>Homepage</h1>
+                    <img src="/images/under-construction.gif" />
+                </body>
+            </html>
+            EOF
+        );
+    }
+
+    #[Route('/conference', name: 'conference')]
+    /**
+     * @Route("/conference", name="conference")
+     */
+    public function conference(): Response
+    {
+        return new Response(<<<EOF
+            <html>
+                <body>
+                    <h1>conference</h1>
                     <img src="/images/under-construction.gif" />
                 </body>
             </html>
